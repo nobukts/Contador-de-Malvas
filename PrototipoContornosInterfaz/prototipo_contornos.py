371ace95e91007f2ebc569576b9a7de8328e49a6
José Osega <jjcoroneljj@gmail.com>
2023-06-30 11:34:59 -0400

    Actualización

--- PrototipoContornosInterfaz/prototipo_contornos.py
+++ PrototipoContornosInterfaz/prototipo_contornos.py
@@ -6,36 +6,48 @@ import cv2
 import imutils
 import numpy as np
 
-def elegir_image():
+def elegir_imagen():
     path_image = filedialog.askopenfilename(filetypes=[("image", ".jpg"),
                                                        ("image", ".jpeg"),
                                                        ("image",".png")])
     if len(path_image) > 0:
         global image
+        #obtención de imagen
         image = cv2.imread(path_image)
         imagen = image
+
+        #redimensionar imagen
         image = imutils.resize(image, height=500)
         imagen = imutils.resize(imagen, height=500)
+
+        #redimensionar imagen que se va colocar en la ventana
         imageToShow = imutils.resize(image, width=500)
-        imageToShow = cv2.cvtColor(imageToShow, cv2.COLOR_BGR2RGB) 
         imageToShow2 = imutils.resize(imagen, width=500)
+
+        #cambiar paleta de colores para utilizar en la ventana
+        imageToShow = cv2.cvtColor(imageToShow, cv2.COLOR_BGR2RGB) 
         imageToShow2 = cv2.cvtColor(imageToShow2, cv2.COLOR_BGR2RGB)
-        bordes = cv2.Canny(imageToShow2, 150, 400)
+
+        #generar bordes de la imagen a analizar
+        bordes = cv2.Canny(imageToShow2, 100, 600)
         contornos, _ = cv2.findContours(bordes, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        #dibujar contornos en la imagen
         cv2.drawContours(imageToShow2, contornos, -1, (0, 255, 0), 2)
 
+        #guardar resultados en variables para mostrar en la ventana
         im = Image.fromarray(imageToShow)
         img = ImageTk.PhotoImage(image=im)
-
         im2 = Image.fromarray(imageToShow2)
         img2 = ImageTk.PhotoImage(image=im2)
 
+        #insertar imagen en la ventana
         lblInputImage1.configure(image=img)
         lblInputImage1.image = img
-
         lblInputImage2.configure(image=img2)
         lblInputImage2.image = img2
-        
+
+        #Insertar texto correspondiente a la imagen        
         lblInfo1 = Label(root, text="IMAGEN ORIGINAL")
         lblInfo2 = Label(root, text="IMAGEN ANALIZADA")
         lblInfo3.config(text=f"Contador: {len(contornos)}")
@@ -43,6 +55,7 @@ def elegir_image():
         lblInfo2.grid(column=1,row=1,padx=5,pady=5)
 
 image = None
+
 root = Tk()
 root.geometry('1000x500+50+50')
 root.title("Contador de malvas")
@@ -52,9 +65,9 @@ lblInputImage1.grid(column=0,row=2)
 lblInputImage2 = Label(root)
 lblInputImage2.grid(column=1,row=2)
 lblInfo3 = Label(root)
-lblInfo3.grid(column=1, row=0)
+lblInfo3.grid(column=1, row=3)
 
-btn = Button(root, text="Elegir imagen", width=25, command=elegir_image)
+btn = Button(root, text="Elegir imagen", width=25, command=elegir_imagen)
 btn.grid(column=0, row=0)
 
 
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Contador de Malvas
 
 Software cuyo propósito es el poder detectar malvas volteadas durante el proceso de bañado de las malvas, para esto se propuso el uso de machine learning para identificar las malvas.
+
+# Google Colab usando YOLO (You Only Look Once) NAS
+
+[![Open In Colab](https://colab.research.google.com/drive/1xMb_IwNNzTeedj3J-mrWF0pvUJffVo-W?usp=sharing)]
